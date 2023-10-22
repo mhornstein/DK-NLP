ede1c01c09d3c8ae4891bbcecb6b78bb955cc877
maor hornstein <mor.h100@gmail.com>
2023-10-22 11:44:59 +0300

    Add data to history tab

--- frontend/src/app/history/history.component.ts
+++ frontend/src/app/history/history.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
-  styleUrls: ['./history.component.scss']
+  styleUrls: ['./history.component.scss', '../app.component.scss']
 })
 export class HistoryComponent {
+  historyData: any[][] = [[["1","hello"],["2","word"],["3","!"]],
+  [["5","bye"],["7","bili"]]];
 
 }
