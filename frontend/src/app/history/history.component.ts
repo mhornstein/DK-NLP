fbadb6e7954a291ab67b257e05724b69f6aa423b
maor hornstein <mor.h100@gmail.com>
2023-10-22 16:45:00 +0300

    update history data structure

--- frontend/src/app/history/history.component.ts
+++ frontend/src/app/history/history.component.ts
@@ -6,8 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./history.component.scss', '../app.component.scss']
 })
 export class HistoryComponent {
-  historyData: any[][] = [[["1","hello"],["2","word"],["3","!"]],
-  [["5","bye"],["7","bili"]]];
+  historyData: any = [
+    {id: 1, date: '2014-03-12T13:37:27+00:00', tagged_sentence: [["1","hello"],["2","word"],["3","!"],["1","hello"],["2","word"],["3","!"],["1","hello"],["2","word"],["3","!"],["1","hello"],["2","word"],["3","!"],["1","hello"],["2","word"],["3","!"],["1","hello"],["2","word"],["3","!"],["1","hello"],["2","word"],["3","!"],["1","hello"],["2","word"],["3","!"],["1","hello"],["2","word"],["3","!"],["1","hello"],["2","word"],["3","!"],["1","hello"],["2","word"],["3","!"],["1","hello"],["2","word"],["3","!"],["1","hello"],["2","word"],["3","!"],["1","hello"],["2","word"],["3","!"],["1","hello"],["2","word"],["3","!"],["1","hello"],["2","word"],["3","!"],["1","hello"],["2","word"],["3","!"],["1","hello"],["2","word"],["3","!"],["1","hello"],["2","word"],["3","!"],["1","hello"],["2","word"],["3","!"],["1","hello"],["2","word"],["3","!"]]},
+    {id: 2, date: '2023-03-12T13:37:27+00:00', tagged_sentence: [["5","bye"],["7","bili"]]}
+  ];
   tagType: string = 'pos'; // Default value
 
 }
