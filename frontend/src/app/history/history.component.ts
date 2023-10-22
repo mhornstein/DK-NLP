920acd3a9ffae8e07b7ead5d08e641ddc9ec166b
maor hornstein <mor.h100@gmail.com>
2023-10-22 11:48:19 +0300

    Add tagging type option

--- frontend/src/app/history/history.component.ts
+++ frontend/src/app/history/history.component.ts
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
 export class HistoryComponent {
   historyData: any[][] = [[["1","hello"],["2","word"],["3","!"]],
   [["5","bye"],["7","bili"]]];
+  tagType: string = 'pos'; // Default value
 
 }
