6e09a5db2df9da4c67b1739176e80f79b9ae7332
maor hornstein <mor.h100@gmail.com>
2023-10-22 11:06:48 +0300

    Add db service

---
+++ frontend/src/app/services/db.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DbService {
+  private data: any[] = [];
+
+  addToDatabase(value: any) {
+    this.data.push(value);
+  }
+
+  getDatabase(): any[] {
+    return this.data;
+  }
+}
--- frontend/src/app/services/tagger.service.ts
+++ frontend/src/app/services/tagger.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
+import { DbService } from './db.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaggerService {
+  constructor(private dbService: DbService) {}
+
   tagText(inputText: string, tagType: string): [string, string][] {
+    debugger
     const words = inputText.split(' ');
-    return words.map((word, index) => [(index + 1).toString(), word]);
+    const results: [string, string][] = words.map((word, index) => [(index + 1).toString(), word]);
+    this.dbService.addToDatabase(results);
+    return results;
   }  
 }
