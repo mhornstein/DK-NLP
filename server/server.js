38257400161708416585129ced661692c0e8bcb1
maor hornstein <mor.h100@gmail.com>
2023-10-24 12:26:57 +0300

    Support tags fetching from dal in server

--- server/server.js
+++ server/server.js
@@ -18,6 +18,20 @@ function validateTagSentenceRequest(req, res, next) {
   }
 }
 
+function validateFetchEntriesRequest(req, res, next) {
+  const { entry_id, num_entries, mode } = req.query;
+
+  if (!mode || (mode !== 'ner' && mode !== 'pos')) {
+    res.status(400).json({ error: 'Invalid mode parameter. It is mandatory and must be "ner" or "pos".' });
+  } else if (num_entries && !Number.isInteger(Number(num_entries))) {
+    res.status(400).json({ error: 'Invalid num_entries parameter. It must be an integer.' });
+  } else if (num_entries && Number.isInteger(Number(num_entries)) && Number(num_entries) <= 0) {
+    res.status(400).json({ error: 'Invalid num_entries parameter. It must be a positive integer.' });
+  } else {
+    next();
+  }
+}
+
 // Routes
 app.get('/tag', validateTagSentenceRequest, async (req, res) => {
   const { mode, sentence } = req.query;
@@ -28,6 +42,27 @@ app.get('/tag', validateTagSentenceRequest, async (req, res) => {
     res.status(500).json({ error: 'Error in tagging service.', details: error.message });
   }
 });
+
+app.get('/fetch_entries', validateFetchEntriesRequest, async (req, res) => {
+  const { entry_id, num_entries, mode } = req.query;
+  let queryParams = `?mode=${mode}`;
+
+  if (entry_id) {
+    queryParams += `&entry_id=${entry_id}`;
+  }
+
+  if (num_entries) {
+    queryParams += `&num_entries=${num_entries}`;
+  }
+
+  try {
+    const response = await axios.get(`http://127.0.0.1:5000/fetch_entries${queryParams}`);
+    res.status(response.status).json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Error in DAL service.', details: error.response.data.error });
+  }
+});
+
 
 // Start the server
 app.listen(port, () => {
