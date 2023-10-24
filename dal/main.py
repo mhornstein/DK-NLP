37245ba234e920f29c08c6c0a336ec9837cb2a92
maor hornstein <mor.h100@gmail.com>
2023-10-24 09:11:15 +0300

    Add support to mode

--- dal/main.py
+++ dal/main.py
@@ -14,13 +14,16 @@ client = MongoClient(MONGO_URI)
 print(f'Listening on: {MONGO_URI}')
 
 DB_NAME = "tags"
-COLLECTION_NAME = "history"
 
 db = client[DB_NAME]  # create the "tags" database if it doesn't exist
 
-if COLLECTION_NAME not in db.list_collection_names(): # Create the 'history' collection if it does not exist
-    db.create_collection(COLLECTION_NAME)
-history_collection = db[COLLECTION_NAME]
+def create_collection_if_not_exists(collection_name):
+    if collection_name not in db.list_collection_names():
+        db.create_collection(collection_name)
+
+# Create collections for 'ner' and 'pos'
+create_collection_if_not_exists("ner_collection")
+create_collection_if_not_exists("pos_collection")
 
 def validate_entry_data(data):
     '''
@@ -35,11 +38,15 @@ def validate_entry_data(data):
     if not isinstance(data, dict):
         return 'Data must be a JSON object'
 
-    if len(data) != 2:
-        return 'Data must have exactly two keys: date and tagged_sentence'
+    if len(data) != 3:
+        return 'Data must have exactly three keys: mode, date, tagged_sentence'
 
-    if 'date' not in data or 'tagged_sentence' not in data:
-        return 'Data must include both date and tagged_sentence'
+    data_keys = list(data.keys())
+    if not all(key in data_keys for key in ['mode', 'date', 'tagged_sentence']):
+        return 'Data keys must include the following keys: mode, date, tagged_sentence'
+
+    if data['mode'] not in ['ner', 'pos']:
+        return 'Invalid mode. Mode should be either "ner" or "pos'
 
     try:
         date = datetime.strptime(data['date'], '%Y-%m-%dT%H:%M:%S%z')
@@ -64,7 +71,7 @@ def add_entry():
     To add a new tag, send a POST request to this endpoint with JSON data containing a date and tagged_sentence.
 
     Example for Curl command:
-        curl -X POST -H "Content-Type: application/json" -d "{\"date\":\"2023-10-24T14:30:00+00:00\",\"tagged_sentence\":[[\"tag11\",\"word1\"],[\"tag2\",\"word2\"]]}" http://localhost:5000/add_entry
+        curl -X POST -H "Content-Type: application/json" -d "{\"date\":\"2023-10-24T14:30:00+00:00\",\"mode\":\"ner\",\"tagged_sentence\":[[\"tag11\",\"word1\"],[\"tag2\",\"word2\"]]}" http://localhost:5000/add_entry
     '''
     try:
         data = request.get_json()
@@ -73,7 +80,9 @@ def add_entry():
         if error_message:
             return jsonify({'error': error_message}), 400
 
+        mode = data['mode']
         data['date'] = datetime.strptime(data['date'], '%Y-%m-%dT%H:%M:%S%z')  # Parse the date string into a datetime object
+        history_collection = db['ner_collection'] if mode == 'ner' else db['pos_collection']
         history_collection.insert_one(data)
         return jsonify({'message': 'Tag added successfully'}), 201
     except Exception as e:
@@ -83,19 +92,31 @@ def add_entry():
 def fetch_entries():
     '''
     To fetch tagged entries, send a GET request to this endpoint.
-    You can provide the 'entry_id' and 'num_entries' query parameters to customize the query.
+
+    Query Parameters:
     - 'entry_id' (optional): The ID of the entry from which to start fetching.
-    - 'num_entries' (optional): The maximal number of entries to fetch (with regard to the db capacity). Default is 10 entries.
-    Example usage with 'curl':
-    curl "http://localhost:5000/fetch_entries"
-    curl "http://localhost:5000/fetch_entries?entry_id=<entry_id>&num_entries=<number>"
+    - 'num_entries' (optional): The maximum number of entries to fetch (with regard to the db capacity). Default is 10 entries.
+    - 'mode' (mandatory): The tagging mode to filter entries by. Options are "ner" or "pos."
+
+    Example Usage with 'curl':
+    - Retrieve 10 entries from a specific mode (e.g., "ner"):
+      curl "http://localhost:5000/fetch_entries?mode=ner"
+
+    - Retrieve 5 entries starting from a specific entry ID and filter by mode:
+      curl "http://localhost:5000/fetch_entries?entry_id=<entry_id>&num_entries=5&mode=pos"
     '''
     try:
         entry_id = request.args.get('entry_id')
         num_entries = int(request.args.get('num_entries', 10))
+        mode = request.args.get('mode')
 
         if not isinstance(num_entries, int) or num_entries <= 0:
             return jsonify({'error': "num_entries must be a positive integer."}), 400
+
+        if mode is None or mode not in ['ner', 'pos']:
+            return jsonify({'error': 'Invalid mode: Mode is mandatory and should be either "ner" or "pos"'}), 400
+
+        history_collection = db[mode + '_collection']
 
         if entry_id is not None and not history_collection.find_one({"_id": ObjectId(entry_id)}):
             return jsonify({'error': "entry_id not found in the database."}), 400
