0341ff180ac28ffce0bf8073bba3d881ecc494fb
maor hornstein <mor.h100@gmail.com>
2023-10-25 15:55:07 +0300

    Add History fetching - part 2: adding load more button

--- frontend/src/app/history/history.component.ts
+++ frontend/src/app/history/history.component.ts
@@ -25,11 +25,14 @@ export class HistoryComponent {
     const historyData = this.historyDataDict[this.tagType];
     if (historyData.length === 0) { // If historyData is empty, call fetchHistory with just tagType
       this.fetchHistory(historyData, this.tagType);
-    } else { // If historyData is not empty, get the last_id and call fetchHistory with both tagType and last_id - TODO: move it to loading button functionality
-      const lastItem = historyData[historyData.length - 1];
+    }
+  }
+
+  reloadHistory() {
+    const historyData = this.historyDataDict[this.tagType];
+    const lastItem = historyData[historyData.length - 1];
       const lastId = lastItem._id;
       this.fetchHistory(historyData, this.tagType, lastId);
-    }
   }
 
   private fetchHistory(historyData: HistoryData[], tagType: string, lastId?: string): void {
@@ -38,5 +41,4 @@ export class HistoryComponent {
       historyData.push(...data);
     });
   }
-
 }
