3d639794cb94638fa08832450a1b8f2c00061488
maor hornstein <mor.h100@gmail.com>
2023-10-26 10:45:03 +0300

    supporting DAL errors in fetching history

--- server/server.js
+++ server/server.js
@@ -123,7 +123,18 @@ app.get('/fetch_entries', validateFetchEntriesRequest, async (req, res) => {
     const response = await axios.get(`http://127.0.0.1:5000/fetch_entries${queryParams}`);
     res.status(response.status).json(response.data);
   } catch (error) {
-    res.status(500).json({ error: 'Error in DAL service.', details: extractErrorDetails(error) });
+    if (error.code == 'ECONNREFUSED') { // The service wasn't avaiable
+      return res.status(500).json({
+        error: 'DAL Service unavailable',
+        details: `Please check the DAL service connection. Details: ${error.message}`,
+      });
+    } else { // this is an error reported by the service
+      const errorDetails = extractErrorDetails(error);
+      return res.status(500).json({
+        error: 'Error reported by DAL Service',
+        details: errorDetails,
+      });
+    }
   }
 });
 
