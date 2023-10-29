666d26625c7630cdba7b81e5af4f7b4db23a009a
maor hornstein <mor.h100@gmail.com>
2023-10-29 20:36:16 +0200

    Add test to add_entry - correct entry added.

---
+++ dal/tests/test_fetch_entries_endpoint.py
@@ -0,0 +1,30 @@
+import sys
+sys.path.append('../')
+
+import unittest
+from unittest.mock import patch
+from app import app
+import app.messages as messages
+
+class TestFetchEntries(unittest.TestCase):
+
+    def setUp(self):
+        app.testing = True
+        self.app = app.test_client()
+
+    @patch('app.routes.get_collection')
+    def test_valid_call(self, mock_get_collection):
+        mocked_db_entry = [{"_id": "653e9b4b7290809194e2801c", "date": "Tue, 24 Oct 2023 14:30:00 GMT",
+             "tagged_sentence": [["tag11", "word1"], ["tag2", "word2"]]}]
+
+        mock_get_collection.return_value.find_one.return_value = 'some returned value'
+        mock_get_collection.return_value.find.return_value.sort.return_value.limit.return_value = mocked_db_entry
+
+        # Make a valid request
+        response = self.app.get('/fetch_entries?mode=ner')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get_json(), mocked_db_entry)
+
+if __name__ == '__main__':
+    unittest.main()
