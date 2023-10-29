af507ca59140533fc58344d3ae0b728e04fc9011
maor hornstein <mor.h100@gmail.com>
2023-10-29 17:38:28 +0200

    Add remaining tests for mode

--- dal/tests/test_routes.py
+++ dal/tests/test_routes.py
@@ -26,7 +26,48 @@ class TestAddEntryEndpoint(unittest.TestCase):
         self.assertIn('message', data)
         mock_get_collection.assert_called_with('pos')  # Ensure the correct mode is used
         mock_get_collection.return_value.insert_one.assert_called_once()
-        assert data['message'] == messages.TAG_ADDED_SUCCESSFULLY
+        self.assertEqual(data['message'], messages.TAG_ADDED_SUCCESSFULLY)
+
+    def test_missing_mode(self):
+        invalid_data = {
+            'date': '2023-10-24T14:30:00+00:00',
+            'tagged_sentence': [['tag11', 'word1'], ['tag2', 'word2']]
+        }
+
+        response = self.app.post('/add_entry', json=invalid_data, content_type='application/json')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('error', data)
+        self.assertEqual(data['error'], messages.INVALID_DATA_KEYS)
+
+    def test_wrong_mode(self):
+        invalid_data = {
+            'date': '2023-10-24T14:30:00+00:00',
+            'mode': 'wrong_mode',
+            'tagged_sentence': [['tag11', 'word1'], ['tag2', 'word2']]
+        }
+
+        response = self.app.post('/add_entry', json=invalid_data, content_type='application/json')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('error', data)
+        self.assertEqual(data['error'], messages.INVALID_MODE)
+
+    def test_mode_as_number(self):
+        invalid_data = {
+            'date': '2023-10-24T14:30:00+00:00',
+            'mode': 123,  # Mode as a number
+            'tagged_sentence': [['tag11', 'word1'], ['tag2', 'word2']]
+        }
+
+        response = self.app.post('/add_entry', json=invalid_data, content_type='application/json')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('error', data)
+        self.assertEqual(data['error'], messages.INVALID_MODE)
 
 if __name__ == '__main__':
     unittest.main()
