42fea5edc8eae270b9212ef31b254b9fbe5dda60
maor hornstein <mor.h100@gmail.com>
2023-10-31 14:41:53 +0200

    reorganize the test

--- server/test/tagRoutes.test.js
+++ server/test/tagRoutes.test.js
@@ -11,15 +11,19 @@ describe('Tagging Route', () => {
   let sandbox;
 
   before(() => {
-    // Mock the axios.get function to return a sample response
-    // Create a sinon sandbox
-    sandbox = sinon.createSandbox();
+    sandbox = sinon.createSandbox(); // To mock the axios instance we will use a sinon sandbox
+  });
 
-    // Mock the axios get and post methods
+  after(() => {
+    sandbox.restore(); // Restore axios to its original implementation
+  });
+
+  it('should simulate a successful GET request', async () => {
+    
+    // Step 1: Mock the axios get and post methods
     const axiosGetStub = sandbox.stub(axios, 'get');
     const axiosPostStub = sandbox.stub(axios, 'post');
 
-    // Define the data to be returned by the mocked axios methods
     const axiosGetResponse = {
       status: 200,
       data: { result: 'tagged_sentence' },
@@ -29,23 +33,16 @@ describe('Tagging Route', () => {
       status: 200,
     };
 
-    // Set up stubs to return the mocked responses
+
     axiosGetStub.withArgs(sinon.match(/127\.0\.0\.1:4000/)).resolves(axiosGetResponse);
     axiosPostStub.withArgs(sinon.match(/127\.0\.0\.1:5000/)).resolves(axiosPostResponse);
-  });
 
-  after(() => {
-    // Restore axios.get to its original implementation
-    sandbox.restore();
-  });
-
-  it('should simulate a successful GET request', async () => {
-    // Perform a simulated GET request
+    // Step 2: Perform a simulated GET request
     const response = await chai.request(server)
       .get('/tag')
       .query({ mode: 'ner', sentence: 'testSentence' });
 
-    // Assertions
+    // Step 3: Assertions
     expect(response).to.have.status(200);
     expect(response.body).to.deep.equal('tagged_sentence');
   });
