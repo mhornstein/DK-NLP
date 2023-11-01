5c1192b8a65e5f1e5673093651beb8b8b99b6e01
maor hornstein <mor.h100@gmail.com>
2023-11-01 11:58:35 +0200

    Add default behaviour for error handling

--- frontend/src/app/services/error-handler.service.ts
+++ frontend/src/app/services/error-handler.service.ts
@@ -14,9 +14,12 @@ export class ErrorHandlerService {
     if (err.status === 0 || err.status === 503) { // server not responding
       header = 'Server Unavailable'
       body = `Please check the server connection. Details: ${err.message}`;
-    } else {
+    } else if (err.error && err.error.error && err.error.details) {
       header = err.error.error;
       body = err.error.details;
+    } else { // unknown error we didn't expect
+      header = 'Unknown Error';
+      body = `Error details: ${JSON.stringify(err)}`;
     }
     this.openErrorDialog(header, body);   
   }
