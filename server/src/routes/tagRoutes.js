129a9112ed2948ad4aa9dd444b6cb2f9b2590909
maor hornstein <mor.h100@gmail.com>
2023-11-01 10:21:48 +0200

    Fix Style in tag route

--- server/src/routes/tagRoutes.js
+++ server/src/routes/tagRoutes.js
@@ -16,7 +16,7 @@ router.get('/tag', requestValidation.validateTagSentenceRequest, async (req, res
   try {
     response = await axios.get(`http://127.0.0.1:4000/tag?mode=${mode}&sentence=${encodeURIComponent(sentence)}`)
   } catch (error) {
-    if (error.code == 'ECONNREFUSED') { // The service wasn't avaiable
+    if (error.code === 'ECONNREFUSED') { // The service wasn't avaiable
       return res.status(500).json({
         error: messages.TAGGING_SERVICE_UNAVAILABLE,
         details: `Please check the tagging service connection. Details: ${error.message}`
@@ -33,34 +33,34 @@ router.get('/tag', requestValidation.validateTagSentenceRequest, async (req, res
   // update the history in the db with the new tagging
   const now = new Date()
   const date = formatISO(now, { representation: 'complete' })
-  const tagged_sentence = response.data.result
+  const taggedSentence = response.data.result
 
   const postData = {
     date,
     mode,
-    tagged_sentence
+    tagged_sentence: taggedSentence
   }
 
   try {
-    const dalResponse = await axios.post('http://127.0.0.1:5000/add_entry', postData, {
+    await axios.post('http://127.0.0.1:5000/add_entry', postData, {
       headers: {
         'Content-Type': 'application/json'
       }
     })
-    return res.status(response.status).json(tagged_sentence)
+    return res.status(response.status).json(taggedSentence)
   } catch (error) {
-    if (error.code == 'ECONNREFUSED') { // The service wasn't avaiable
+    if (error.code === 'ECONNREFUSED') { // The service wasn't avaiable
       return res.status(503).json({
         error: messages.DAL_SERVICE_UNAVAILABLE,
         details: `Please check the DAL service connection. Details: ${error.message}`,
-        tagged_sentence
+        tagged_sentence: taggedSentence
       })
     } else { // this is an error reported by the service
       const errorDetails = extractErrorDetails(error)
       return res.status(503).json({
         error: messages.DAL_SERVICE_ERROR,
         details: errorDetails,
-        tagged_sentence
+        tagged_sentence: taggedSentence
       })
     }
   }
