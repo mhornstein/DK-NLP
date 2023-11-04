93ce5f01d344070eee769a2fab8622d5678dd4be
maor hornstein <mor.h100@gmail.com>
2023-11-04 20:02:19 +0200

    Add test to navbar test

--- frontend/src/app/navbar/navbar.component.spec.ts
+++ frontend/src/app/navbar/navbar.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NavbarComponent } from './navbar.component';
+import { By } from '@angular/platform-browser';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -18,5 +19,15 @@ describe('NavbarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have links for "Tagger", "History", and "About"', () => {
+    const links = fixture.debugElement.queryAll(By.css('a'));
+    expect(links.length).toBe(3);
+
+    const linkTexts = links.map((link) => link.nativeElement.textContent);
+    expect(linkTexts).toContain('Tagger');
+    expect(linkTexts).toContain('History');
+    expect(linkTexts).toContain('About');
   });
 });
