13c9e0769a7f65fca09d76691d62e25ee7417d8e
maor hornstein <mor.h100@gmail.com>
2023-11-06 11:35:25 +0200

    Update validation error to be of the convention of error + details dictionary

--- server/src/middleware/requestValidation.js
+++ server/src/middleware/requestValidation.js
@@ -4,7 +4,7 @@ function validateTagSentenceRequest (req, res, next) {
   const { mode, sentence } = req.query
 
   if (!mode || (mode !== 'ner' && mode !== 'pos') || !sentence) {
-    res.status(400).json({ error: messages.INVALID_TAG_REQUEST })
+    res.status(400).json({ error: messages.INVALID_PARAMETER_ERROR, details: messages.INVALID_TAG_REQUEST })
   } else {
     next()
   }
@@ -15,11 +15,11 @@ function validateFetchEntriesRequest (req, res, next) {
   const numEntries = req.query.num_entries
 
   if (!mode || (mode !== 'ner' && mode !== 'pos')) {
-    res.status(400).json({ error: messages.ILLEGAL_OR_MISSING_MODE })
+    res.status(400).json({ error: messages.INVALID_PARAMETER_ERROR, details: messages.ILLEGAL_OR_MISSING_MODE })
   } else if (numEntries && !Number.isInteger(Number(numEntries))) {
-    res.status(400).json({ error: messages.NUM_ENTRIES_MUST_BE_INT })
+    res.status(400).json({ error: messages.INVALID_PARAMETER_ERROR, details: messages.NUM_ENTRIES_MUST_BE_INT })
   } else if (numEntries && Number.isInteger(Number(numEntries)) && Number(numEntries) <= 0) {
-    res.status(400).json({ error: messages.NUM_ENTRIES_MUST_BE_POSITIVE })
+    res.status(400).json({ error: messages.INVALID_PARAMETER_ERROR, details: messages.NUM_ENTRIES_MUST_BE_POSITIVE })
   } else {
     next()
   }
--- server/src/util/messages.js
+++ server/src/util/messages.js
@@ -1,4 +1,5 @@
 module.exports = {
+  INVALID_PARAMETER_ERROR: 'Invalide parameter error',
   INVALID_TAG_REQUEST: 'Invalid mode or sentence parameters. Mode must be "ner" or "pos". Sentence is required.',
   ILLEGAL_OR_MISSING_MODE: 'Invalid mode parameter. It is mandatory and must be "ner" or "pos".',
   NUM_ENTRIES_MUST_BE_POSITIVE: 'Invalid num_entries parameter. It must be an integer and a positive number.',
