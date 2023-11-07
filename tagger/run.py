e635a184ca491121658227023388da00b10050cf
maor hornstein <mor.h100@gmail.com>
2023-11-07 11:54:56 +0200

    Make run take command line arguments

--- tagger/run.py
+++ tagger/run.py
@@ -1,4 +1,11 @@
 from app import *
+import argparse
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=4000)
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Tagging Microservice")
+    parser.add_argument("--port", type=int, default=4000, help="Port to expose the tagging microservice")
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    app.run(host='0.0.0.0', port=args.port)
