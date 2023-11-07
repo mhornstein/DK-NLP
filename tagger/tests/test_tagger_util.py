3aa4fa4bb1424fafeef2236156c5d2cdcfa3994f
maor hornstein <mor.h100@gmail.com>
2023-11-07 09:16:45 +0200

    Add tests to tagger_util

---
+++ tagger/tests/test_tagger_util.py
@@ -0,0 +1,44 @@
+import sys
+
+sys.path.append("../app")
+
+import unittest
+from app.tagger_util import Tagger
+import app.bilstm_modules as bilstm_modules
+
+class TestTagger(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # Initialize the taggers before running the tests
+        cls.pos_tagger = Tagger(
+            model_path="../models/pos_model/model.pth",
+            vocab_file="../models/pos_model/vocabs",
+            dicts_file="../models/pos_model/dicts"
+        )
+        cls.ner_tagger = Tagger(
+            model_path="../models/ner_model/model.pth",
+            vocab_file="../models/ner_model/vocabs",
+            dicts_file="../models/ner_model/dicts"
+        )
+
+    def test_tag_pos(self):
+        sentence = "This is a test sentence."
+        tags = self.pos_tagger.tag(sentence)
+        expected_tags = [['This', 'DT'], ['is', 'VBZ'], ['a', 'DT'], ['test', 'NN'], ['sentence', 'NN'], ['.', '.']]
+        self.assertIsInstance(tags, list)
+        self.assertEqual(tags, expected_tags)
+
+    def test_tag_ner(self):
+        sentence = "Google is an organization based in San Francisco, California."
+        tags = self.ner_tagger.tag(sentence)
+        expected_tags = [['Google', 'ORG'], ['is', 'O'], ['an', 'O'], ['organization', 'O'], ['based', 'O'], ['in', 'O'], ['San', 'LOC'], ['Francisco', 'LOC'], [',', 'O'], ['California', 'LOC'], ['.', 'O']]
+        self.assertIsInstance(tags, list)
+        self.assertEqual(tags, expected_tags)
+
+    def test_tag_long_sentence(self):
+        sentence = " ".join(["word"] * 200)
+        with self.assertRaises(Exception):
+            self.pos_tagger.tag(sentence)
+
+    def tearDown(self):
+        pass
