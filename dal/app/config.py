0677190d4218def4df09cb312c1d2fcda3d4fa17
maor hornstein <mor.h100@gmail.com>
2023-11-07 11:42:33 +0200

    Make run take command line arguments

--- dal/app/config.py
+++
@@ -1,3 +0,0 @@
-import os
-
-MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017")
--- dal/run.py
+++ dal/run.py
@@ -1,4 +1,13 @@
 from app import *
+import argparse
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Dal Microservice")
+    parser.add_argument("--port", type=int, default=5000, help="Port to expose the dal microservice")
+    parser.add_argument("--mongo-uri", default="mongodb://localhost:27017", help="MongoDB URI")
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    app.config["MONGO_URI"] = args.mongo_uri  # Set the MongoDB URI from the command line
+    app.run(host='0.0.0.0', port=args.port)
