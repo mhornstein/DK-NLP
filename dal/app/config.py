0b14ecb2f1eff5ea9209c7a2d00d9e67bb675031
maor hornstein <mor.h100@gmail.com>
2023-11-07 11:23:27 +0200

    Revert "Making port to be configurable"
    This reverts commit 214da57888c0444bbb51389ab9f6ef62cdc141fe.

--- dal/app/config.py
+++ dal/app/config.py
@@ -1,4 +1,3 @@
 import os
 
 MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017")
-PORT = int(os.environ.get('PORT', 5000))
--- dal/run.py
+++ dal/run.py
@@ -1,5 +1,4 @@
 from app import *
-from app.config import PORT
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=PORT)
+    app.run(host="0.0.0.0", port=5000)
