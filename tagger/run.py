bc9832d0efbcd72408680e38d3b2d5d0ac5b27cc
maor hornstein <mor.h100@gmail.com>
2023-11-08 17:30:06 +0200

    Add Swagger - part 3: make Swagger optional via cmd

--- tagger/run.py
+++ tagger/run.py
@@ -5,11 +5,14 @@ from flasgger import Swagger
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Tagging Microservice")
     parser.add_argument("--port", type=int, default=4000, help="Port to expose the tagging microservice")
+    parser.add_argument("--enable-api", action="store_true", help="Enable Swagger API documentation")
     return parser.parse_args()
 
 if __name__ == '__main__':
     args = parse_arguments()
 
-    swagger = Swagger(app, template_file='../swagger/api-docs.yaml')
+    if args.enable_api:
+        swagger = Swagger(app, template_file='../swagger/api-docs.yaml')
+        print(f"Swagger UI is enabled and can be accessed at /apidocs")
 
     app.run(host='0.0.0.0', port=args.port)
