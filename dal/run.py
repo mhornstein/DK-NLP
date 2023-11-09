0c81e694c87d065eaaf42ec7582e22b24b8ea822
maor hornstein <mor.h100@gmail.com>
2023-11-09 09:39:00 +0200

    Add swagger to dal - part 3: make it configurable via cmd

--- dal/run.py
+++ dal/run.py
@@ -6,10 +6,13 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description="Dal Microservice")
     parser.add_argument("--port", type=int, default=5000, help="Port to expose the dal microservice")
     parser.add_argument("--mongo-uri", default="mongodb://localhost:27017", help="MongoDB URI")
+    parser.add_argument("--enable-api", action="store_true", help="Enable Swagger API documentation")
     return parser.parse_args()
 
 if __name__ == '__main__':
     args = parse_arguments()
     app.config["MONGO_URI"] = args.mongo_uri  # Set the MongoDB URI from the command line
-    swagger = Swagger(app, template_file='../swagger/api-docs.yaml')
+    if args.enable_api:
+        swagger = Swagger(app, template_file='../swagger/api-docs.yaml')
+        print(f"Swagger UI is enabled and can be accessed at /apidocs")
     app.run(host='0.0.0.0', port=args.port)
