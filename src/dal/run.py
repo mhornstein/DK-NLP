d56078328dbafa3c66f4115cdfeafd72dc11ad65
maor hornstein <mor.h100@gmail.com>
2023-11-12 20:02:25 +0200

    rename swagger => docs folder

--- src/dal/run.py
+++ src/dal/run.py
@@ -13,6 +13,6 @@ if __name__ == '__main__':
     args = parse_arguments()
     app.config["MONGO_URI"] = args.mongo_uri  # Set the MongoDB URI from the command line
     if args.enable_api:
-        swagger = Swagger(app, template_file='../swagger/api-docs.yaml')
+        swagger = Swagger(app, template_file='../docs/api-docs.yaml')
         print(f"Swagger UI is enabled and can be accessed at /apidocs")
     app.run(host='0.0.0.0', port=args.port)
