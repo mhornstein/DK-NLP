d3a62cc138419cb275e68317448159d2e6e3ce93
maor hornstein <mor.h100@gmail.com>
2023-11-18 17:15:56 +0200

    Adding option to serve via waitress for both dal and tagger

--- src/dal/src/run.py
+++ src/dal/src/run.py
@@ -1,6 +1,7 @@
 from app import create_app
 import argparse
 from flasgger import Swagger
+from waitress import serve
 
 
 def parse_arguments():
@@ -8,6 +9,12 @@ def parse_arguments():
     parser.add_argument("--port", type=int, default=5000, help="Port to expose the dal microservice")
     parser.add_argument("--mongo-uri", default="mongodb://localhost:27017", help="MongoDB URI")
     parser.add_argument("--enable-api", action="store_true", help="Enable Swagger API documentation")
+    parser.add_argument(
+        "--serve",
+        default="dev",
+        choices=["prod", "dev"],
+        help="Serve mode (prod for production, dev for development)",
+    )
     return parser.parse_args()
 
 
@@ -19,4 +26,10 @@ if __name__ == "__main__":
     if args.enable_api:
         swagger = Swagger(app, template_file="./docs/api-docs.yaml")
         print("Swagger UI is enabled and can be accessed at /apidocs")
-    app.run(host="0.0.0.0", port=args.port)
+    serve_mode = args.serve
+    if serve_mode == "prod":
+        print(f"Running in production mode on port {args.port}")
+        serve(app, host="0.0.0.0", port=args.port)
+    else:
+        print(f"Running in development mode on port {args.port}")
+        app.run(host="0.0.0.0", port=args.port)
--- src/tagger/src/run.py
+++ src/tagger/src/run.py
@@ -1,12 +1,19 @@
 import argparse
 from flasgger import Swagger
 from app import create_app
+from waitress import serve
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Tagging Microservice")
     parser.add_argument("--port", type=int, default=4000, help="Port to expose the tagging microservice")
     parser.add_argument("--enable-api", action="store_true", help="Enable Swagger API documentation")
+    parser.add_argument(
+        "--serve",
+        default="dev",
+        choices=["prod", "dev"],
+        help="Serve mode (prod for production, dev for development)",
+    )
     return parser.parse_args()
 
 
@@ -18,4 +25,10 @@ if __name__ == "__main__":
         swagger = Swagger(app, template_file="./docs/api-docs.yaml")
         print("Swagger UI is enabled and can be accessed at /apidocs")
 
-    app.run(host="0.0.0.0", port=args.port)
+    serve_mode = args.serve
+    if serve_mode == "prod":
+        print(f"Running in production mode on port {args.port}")
+        serve(app, host="0.0.0.0", port=args.port)
+    else:
+        print(f"Running in development mode on port {args.port}")
+        app.run(host="0.0.0.0", port=args.port)
